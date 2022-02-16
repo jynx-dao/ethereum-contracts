f0478af18a4033df2289d5298707bda026219bd5
First Excited State <firstexcitedstate@protonmail.com>
2022-02-16 11:44:43 +0000

    placeholder tests for distribution

--- test/JYNX_Distribution_Test.js
+++ test/JYNX_Distribution_Test.js
@@ -2,5 +2,31 @@ const JYNX_Distribution = artifacts.require("JYNX_Distribution");
 const JYNX = artifacts.require("JYNX");
 
 contract("JYNX_Distribution", (accounts) => {
-
+  describe("initialize", async () => {
+    it("should...", async () => {});
+  });
+  describe("create_distribution", async () => {
+    it("should...", async () => {});
+  });
+  describe("buy_tokens", async () => {
+    it("should...", async () => {});
+  });
+  describe("reclaim_unsold_tokens", async () => {
+    it("should...", async () => {});
+  });
+  describe("update_dai_address", async () => {
+    it("should...", async () => {});
+  });
+  describe("redeem_erc20", async () => {
+    it("should...", async () => {});
+  });
+  describe("claim_tokens_for_distribution", async () => {
+    it("should...", async () => {});
+  });
+  describe("claim_treasury_tokens", async () => {
+    it("should...", async () => {});
+  });
+  describe("claim_network_tokens", async () => {
+    it("should...", async () => {});
+  });
 });
