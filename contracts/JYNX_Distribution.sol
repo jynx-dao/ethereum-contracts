ad3f05f67f1b207531177cbfd42b81dbc2f0d739
First Excited State <firstexcitedstate@protonmail.com>
2022-02-16 09:12:38 +0000

    added function to claim tokens

--- contracts/JYNX_Distribution.sol
+++ contracts/JYNX_Distribution.sol
@@ -15,6 +15,7 @@ contract JYNX_Distribution is Ownable {
   uint256 public network_pool;
   uint256 public community_pool;
 
+  mapping(uint8 => mapping(address => uint256)) public claimed_tokens;
   mapping(uint8 => mapping(address => uint256)) public user_allocations;
   mapping(uint8 => Distribution) public distribution_events;
   uint8 distribution_count = 0;
@@ -79,9 +80,9 @@ contract JYNX_Distribution is Ownable {
     require(distribution_events[id].start_date < block.timestamp, "token sale not started");
     require(distribution_events[id].end_date > block.timestamp, "token sale ended");
     require(distribution_events[id].total_tokens - distribution_events[id].tokens_sold > 0, "sold out");
-    dai.transferFrom(msg.sender, address(this), amount);
     uint256 token_amount = amount / distribution_events[id].usd_rate;
     user_allocations[id][msg.sender] += token_amount;
+    dai.transferFrom(msg.sender, address(this), amount);
   }
 
   function reclaim_unsold_tokens(
@@ -109,5 +110,38 @@ contract JYNX_Distribution is Ownable {
     uint256 balance = ERC20(erc20_address).balanceOf(address(this));
     require(balance >= amount, "insufficient balance");
     ERC20(erc20_address).transfer(destination, amount);
+  }
+
+  function claim_tokens(
+    uint8 id
+  ) public {
+    require(distribution_events[id].end_date < block.timestamp, "token sale has not ended");
+    require(distribution_events[id].cliff_timestamp < block.timestamp, "cliff is not in the past");
+    uint256 fully_vested_timestamp = distribution_events[id].cliff_timestamp + distribution_events[id].vesting_duration;
+    uint256 available_tokens = 0;
+    if(block.timestamp > fully_vested_timestamp) {
+      available_tokens = user_allocations[id][msg.sender] - claimed_tokens[id][msg.sender];
+    } else {
+      uint256 seconds_since_cliff = block.timestamp - distribution_events[id].cliff_timestamp;
+      uint256 vested_ratio = (seconds_since_cliff * 1000000) / distribution_events[id].vesting_duration;
+      uint256 vested_tokens = (user_allocations[id][msg.sender] * vested_ratio) / 1000000;
+      available_tokens = vested_tokens - claimed_tokens[id][msg.sender];
+    }
+    claimed_tokens[id][msg.sender] += available_tokens;
+    jynx_token.transfer(msg.sender, available_tokens);
+  }
+
+  function get_claimed_tokens(
+    uint8 id,
+    address user
+  ) public view returns(uint256) {
+    return claimed_tokens[id][user];
+  }
+
+  function get_unclaimed_tokens(
+    uint8 id,
+    address user
+  ) public view returns(uint256) {
+    return user_allocations[id][user] - get_claimed_tokens(id, user);
   }
 }
